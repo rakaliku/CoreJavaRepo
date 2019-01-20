694107c38027390fe6fd5becd13fb6ef16c1d169
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-20 22:18:52 +0530

    1 method added in Employee class

--- src/com/java/conceptjavaclass/Employee.java
+++ src/com/java/conceptjavaclass/Employee.java
@@ -5,4 +5,10 @@ public class Employee {
 	
 	int num1=251;
 	int num2=87;
+	String methodOfEmployee()
+	{
+		System.out.println("Employee class method and return type is STRING");
+		return "rakesh";
+		
+	}
 }
