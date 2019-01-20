b68ead2a51cdfdf4a8660ba927be5a509e906eb5
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-20 22:09:07 +0530

    New 2 classes added

---
+++ src/com/java/conceptjavaclass/Employee.java
@@ -0,0 +1,8 @@
+package com.java.conceptjavaclass;
+
+public class Employee {
+
+	
+	int num1=251;
+	int num2=87;
+}
---
+++ src/com/java/conceptjavaclass/ReturnOfUserDefinedClassVariableCode1.java
@@ -0,0 +1,19 @@
+package com.java.conceptjavaclass;
+
+public class ReturnOfUserDefinedClassVariableCode1 {
+
+	Employee multipicationMethoD(int m1, int m2)
+	{
+		int mult=m1*m2;
+		System.out.println(mult);
+		Employee e=new Employee();
+		return e;
+	}
+	public static void main(String[] args) {
+		
+		new ReturnOfUserDefinedClassVariableCode1().multipicationMethoD(50, 4);
+		System.out.println(new Employee().num1);/* Employee class variable 
+		printing statement */
+	}
+
+}
