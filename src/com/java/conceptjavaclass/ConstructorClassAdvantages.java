b2ddbcde6ce26be713283ff0f3a3de2f5d82bb96
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-21 10:20:21 +0530

    constructor assign some some values to reference object variAble to some Random values

---
+++ src/com/java/conceptjavaclass/ConstructorClassAdvantages.java
@@ -0,0 +1,23 @@
+package com.java.conceptjavaclass;
+
+public class ConstructorClassAdvantages {
+
+	
+	int eid;
+	String ename;
+	double esal;
+	void display()
+	{
+		System.out.println("****employee details*******");
+		System.out.println("employee name "+ename);
+		System.out.println("employee salary"+esal);
+		System.out.println("employee id"+eid);
+		
+		
+	}
+	public static void main(String[] args)
+	{
+		ConstructorClassAdvantages e1= new ConstructorClassAdvantages();
+		e1.display();
+	}
+}
