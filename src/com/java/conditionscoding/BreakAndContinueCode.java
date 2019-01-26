982acb90648e07335e925fab97510918138ab341
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-26 21:18:10 +0530

    break statement added

--- src/com/java/conditionscoding/BreakAndContinueCode.java
+++ src/com/java/conditionscoding/BreakAndContinueCode.java
@@ -8,10 +8,24 @@ public class BreakAndContinueCode {
 		int x=1;
 		if (x==5)
 		{
-			continue;
+			if(x>2)
+			//	continue;
 			System.out.println(x);
 			x++;
 			
 		}
+	}
+	static void breakMethod()
+	{
+		for (int y=0;y<4;y++)
+		{
+			if(y==2)
+				break;
+			System.out.println(y);
+		}
+	}
+	public static void main(String[] args)
+	{
+		breakMethod();
 	}
 }
