0b613a4158b26d2d94abf1eb91f593b54ebb1ccc
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-26 21:13:07 +0530

    continue statement code...........but it's wrong

---
+++ src/com/java/conditionscoding/BreakAndContinueCode.java
@@ -0,0 +1,17 @@
+package com.java.conditionscoding;
+
+public class BreakAndContinueCode {
+
+	
+	static void  method()
+	{
+		int x=1;
+		if (x==5)
+		{
+			continue;
+			System.out.println(x);
+			x++;
+			
+		}
+	}
+}
