6fad2e505dbc5047f621a853a8297cba25767763
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-28 10:17:41 +0530

    OOPS concept codings

---
+++ src/oopscodings/CollegeClassDetails.java
@@ -0,0 +1,14 @@
+package oopscodings;
+
+public class CollegeClassDetails {
+
+	void department()
+	{
+		System.out.println("department is mechanical");
+	}
+	void admin()
+	{
+		department();
+		System.out.println("admin method statement");
+	}
+}
---
+++ src/oopscodings/InheritanceCoding.java
@@ -0,0 +1,23 @@
+package oopscodings;
+
+class College
+{
+	void admin()
+	{
+		System.out.println("admin method statement");
+	}
+	 
+	void department()
+	{
+		System.out.println("department method");
+	}
+}
+public class InheritanceCoding {
+
+	 
+	
+	public static void main(String [] args)
+	{
+	new College().admin();	
+	}
+}
