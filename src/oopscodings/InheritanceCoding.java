eaf08ce9551012fec1a8c563af107ce1457ab9bb
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-28 10:47:44 +0530

    different package is inherited

--- src/oopscodings/InheritanceCoding.java
+++ src/oopscodings/InheritanceCoding.java
@@ -1,14 +1,24 @@
+
 package oopscodings;
 
+//import com.java.stringexamples;----Its wrong , because I have not given * or class name which will be used in below coding
+import com.java.stringexamples.StringPrinting;
 
-public class InheritanceCoding {
+public class InheritanceCoding extends StringPrinting  {
 
 	 
 	
 	public static void main(String [] args)
 	{
-		//below CollegeClassDetails class is in different class still  
-		//no need of extends the class to use
+//below CollegeClassDetails class is in different class of same package still  
+//no need of extends the class to use
 	new CollegeClassDetails().admin();	
+
+/*below 	StringPrinting class is defined in different package i.e com.java.stringexample
+still no extend keyword required to use this package's class but method should be public
+*/	new StringPrinting().method();
+	/*Why here method needs to public in its class even after the respective
+	class in extended here for inheritance*/
+	
 	}
 }
