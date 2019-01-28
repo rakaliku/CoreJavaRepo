2e8177b19d7113669ddf017174ceab23c5dcf561
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-28 08:55:08 +0530

    instance block will be execute first then constructor in top to bottom approach.

--- src/com/java/conceptjavaclass/InstanceBlockCode.java
+++ src/com/java/conceptjavaclass/InstanceBlockCode.java
@@ -10,6 +10,9 @@ public class InstanceBlockCode {
 	{
 		System.out.println("constructor statement");
 	}
+	{
+		System.out.println("instance block after the constructor block but its printing before theconstructor only ");
+	}
 	public static void main(String [] args)
 	{
 		new InstanceBlockCode();
