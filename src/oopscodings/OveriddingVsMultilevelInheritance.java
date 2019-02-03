a3882865cfdda716e5c50e6e552c6204b2e62818
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-02-03 22:38:05 +0530

    Overriding coding

---
+++ src/oopscodings/OveriddingVsMultilevelInheritance.java
@@ -0,0 +1,42 @@
+package oopscodings;
+
+class Person
+{
+	void eat()
+	{
+		System.out.println("4-idly");
+	}
+	
+}
+class Ratan extends Person
+{
+	void eat()
+	{
+		System.out.println("10 -idly");
+	}
+}
+class RatanKid extends Ratan
+{
+	void eat()
+	{
+		System.out.println("2 -idly");
+	}
+}
+public class OveriddingVsMultilevelInheritance {
+
+	public static void main(String [] args)
+	{
+		Person p=new Ratan();
+		p.eat();//10-idly
+		
+		Ratan r=new RatanKid();
+		r.eat();//2-idly
+		
+		Person p1=new RatanKid();
+		p1.eat();//2-idly
+		
+		//RatanKid r1=new Ratan();
+		//CHILD CLASS OBJECT CAN NOT HOLD PARENT CLASS
+				
+	}
+}
