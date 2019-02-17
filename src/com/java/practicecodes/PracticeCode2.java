8501c5f8b9eefd47e8572ac9ea8eb68b99c62ca8
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-02-17 21:54:32 +0530

    Print statement inside the TRY and CATCH block is not possible But after try catch block we can give PRINT Statement
    Inthis code NUMBER variable is having only used inside the try block
    outside it can not be used

---
+++ src/com/java/practicecodes/PracticeCode2.java
@@ -0,0 +1,21 @@
+package com.java.practicecodes;
+
+public class PracticeCode2 {
+
+	public static void main(String [] args)
+	{
+		try {
+			Double number =Double.valueOf(12D);
+			
+		}
+	//	System.out.println("inside statement");
+		catch(NumberFormatException ex)
+		{
+			System.out.println(ex);
+			
+		}
+		//System.out.println(number);
+		System.out.println("print staement of outised TRY AND CATCH BLOCK");
+	//number variable is only having the scope in TRY Block
+	}
+}
