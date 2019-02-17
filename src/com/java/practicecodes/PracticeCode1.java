95dbf5baa51e2566a806a9aa6337905c7472b7d5
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-02-17 21:43:55 +0530

    compilation error will come as x is not declared in local if only x will be declared in for loop

---
+++ src/com/java/practicecodes/PracticeCode1.java
@@ -0,0 +1,17 @@
+package com.java.practicecodes;
+
+public class PracticeCode1 {
+
+	public static void main(String[] args)
+	{
+		int sum=0;
+		//int x;
+		for (int x=0; x<=10;x++)
+			sum +=x;
+	//	System.out.println("Sum for 0 to "+ x);
+		
+	//compilation error will come as x is not declared in local	
+		
+		System.out.println("="+sum);
+	}
+}
