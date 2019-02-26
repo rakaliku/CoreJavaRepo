64bb7eac95d0993b5526e46c608e81792c0c60b2
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-02-26 09:02:03 +0530

    Type casting coding . Explicit type casting is performed by USER and Implicit type casting is done by COMPILER

---
+++ src/com/java/oopscodings/TypeCstingCode.java
@@ -0,0 +1,31 @@
+package com.java.oopscodings;
+
+public class TypeCstingCode {
+
+	
+	public static void main(String [] args)
+	{
+		byte b=10;
+		short s=9;
+		int i=s;
+		System.out.println(i);
+		System.out.println(b);
+		
+		float n= 598;
+		double d=n;
+		System.out.println(d);
+		
+		char ch='z';
+		double d1=ch;
+		System.out.println(d1);
+		
+		//Explicit type casting is done by USER as below
+		double d2=52.36;
+		float num=(float)d2;
+		System.out.println(num);
+		
+		float f2=54;
+		short s2=(short)f2;
+		System.out.println(s2);
+	}
+}
