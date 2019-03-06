e08ac8b3b341a01a4fdc1c0de94a63e44571d16a
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-03-06 10:45:11 +0530

    exceptional handiling practice code

---
+++ src/com/java/exceptionalhandlingcodes/ExceptionalHandlingCode1.java
@@ -0,0 +1,36 @@
+package com.java.exceptionalhandlingcodes;
+
+public class ExceptionalHandlingCode1 {
+
+	public static void main(String [] args)
+	{
+		try
+		{
+			System.out.println("     try block ");
+			try
+			{
+				int[] a= {5,6,7};
+				System.out.println(a[5]);
+			}catch(ArrayIndexOutOfBoundsException a1)
+			{
+				int[] a= {5,6,7};
+				System.out.println(a[1]);
+				System.out.println("");
+			}
+			
+			System.out.println(10/0);
+			
+		}
+		catch(ArithmeticException  ae)
+		{
+			System.out.println("     catch block     ");
+			System.out.println(10/2);
+		}
+		finally
+		{
+			System.out.println("finally block stement");
+		}
+	}
+	
+	
+}
