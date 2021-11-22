3aef7d85daf4abd8f089b5ded647ca2f01590671
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2021-11-22 16:34:12 +0530

    for normal testing

--- src/com/java/practicecodes/PracticeCode1.java
+++ src/com/java/practicecodes/PracticeCode1.java
@@ -5,10 +5,11 @@ public class PracticeCode1 {
 	public static void main(String[] args)
 	{
 		int sum=0;
-		//int x;
+//		int x;
+		System.out.println("sum");
 		for (int x=0; x<=10;x++)
 			sum +=x;
-	//	System.out.println("Sum for 0 to "+ x);
+//		System.out.println("Sum for 0 to "+ x);
 		
 	//compilation error will come as x is not declared in local	
 		
