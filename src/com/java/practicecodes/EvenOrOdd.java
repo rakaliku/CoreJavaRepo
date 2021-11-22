359272b68f62b5c24b2d46a1dfda39ef5e5cfc1b
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2021-11-22 19:39:59 +0530

    A number is Even or odd logic

---
+++ src/com/java/practicecodes/EvenOrOdd.java
@@ -0,0 +1,16 @@
+package com.java.practicecodes;
+
+public class EvenOrOdd {
+    public static void main(String[] args)
+    {
+        int a = 13;
+        if (a%2 == 0)
+        {
+            System.out.println("A is Even.....");
+        }
+        else
+        {
+            System.out.println("A is Odd........");
+        }
+    }
+}
