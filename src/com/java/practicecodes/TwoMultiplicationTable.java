f3c45e1892738dd8f00f5a60b6a1066c01ee08c6
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2021-11-26 19:02:15 +0530

    2 multiplication table

---
+++ src/com/java/practicecodes/TwoMultiplicationTable.java
@@ -0,0 +1,12 @@
+package com.java.practicecodes;
+
+public class TwoMultiplicationTable {
+    public static void main(String[] args)
+    {
+        int n = 2;
+        for (int i =1;i<=10;i++)
+        {
+        System.out.println(n+"*"+i+"="+n*i);
+        }
+    }
+}
