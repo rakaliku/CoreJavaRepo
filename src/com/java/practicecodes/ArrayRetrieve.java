9d0d9fe30c923fb75a9a969523e62db94af82d2f
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2021-12-10 22:23:07 +0530

    array

---
+++ src/com/java/practicecodes/ArrayRetrieve.java
@@ -0,0 +1,15 @@
+package com.java.practicecodes;
+
+public class ArrayRetrieve {
+    public static void main(String[] args)
+    {
+        int[] arr = new int[]{12,34,43,54,65};
+        System.out.println(arr);
+        System.out.println(arr.length);
+        for (int i =0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
