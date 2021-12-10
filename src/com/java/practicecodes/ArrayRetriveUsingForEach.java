7dca06ae67b490aedd6b87baf0ff074a193d57c9
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2021-12-10 22:58:49 +0530

    array using for each

---
+++ src/com/java/practicecodes/ArrayRetriveUsingForEach.java
@@ -0,0 +1,13 @@
+package com.java.practicecodes;
+
+public class ArrayRetriveUsingForEach {
+    public static void main(String[] args)
+    {
+        int[] arr = new int[]{10,2,3,49};
+        for (int i:arr)
+        {
+            System.out.println(i);
+
+        }
+    }
+}
